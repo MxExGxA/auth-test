644830bf540c72cc8c80b6c9b46b59922c6b845e
MxExGxA <elsayedelghazy20@gmail.com>
2024-11-25 01:24:57 +0200

    second commit

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "client")));
 
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
